bb582074204247930523f16805b2defcc62985e9
binoykumerdasgupta <binoykdasgupta@gmail.com>
2022-04-19 21:47:34 +0100

    multiplication function added

--- hello.js
+++ hello.js
@@ -6,6 +6,8 @@ const sum = a+b;
 const subs = a-b;
 console.info('The sum of A and B is:'+ sum)
 console.info('The subs of A and B is:'+ subs)
+console.info('The multiples of A and B is:'+ (a * b))
+
 
 
 
